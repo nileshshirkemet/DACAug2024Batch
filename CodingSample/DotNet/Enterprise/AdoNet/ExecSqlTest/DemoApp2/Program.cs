208393bde1e7ce81a27c1188cbc99a3d71af025a
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2024-10-19 11:46:38 +0530

    Add files via upload

---
+++ CodingSample/DotNet/Enterprise/AdoNet/ExecSqlTest/DemoApp2/Program.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.Data.SqlClient;
+
+using var connection = new SqlConnection("Data Source=iitdac.met.edu;Database=Shop;User Id=dac;Password=Dac@1234;Trust Server Certificate=True");
+connection.Open();
+
